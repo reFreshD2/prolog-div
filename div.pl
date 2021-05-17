a13e1f73a209aea9c854c5dcf9db9e0b184b13f0
Vladimir <44308263+reFreshD2@users.noreply.github.com>
2021-05-17 19:17:28 +1000

    Change

--- div.pl
+++ div.pl
@@ -1,4 +1,21 @@
 length([], Length, Length).
-length([_Head|Tail], Buffer, Length):-NewBuffer is Buffer + 1, length(Tail, NewBuffer, Length).
-div(List,[]):-length(List,0,Length), Length = 1.
-div([El1,El2|Torig],[H|Tdiv]):-Div1 is El1 - El2, Div1 = H, div([El2|Torig],Tdiv).
+length([_Head|Tail], Buffer, Length):-
+    NewBuffer is Buffer + 1,
+    length(Tail, NewBuffer, Length).
+div(List,Div):-
+    length(List,0,LenList), 
+    length(Div,0,LenDiv),
+    LenListBuff is LenList/2-1,
+    LenListBuff = LenDiv, 
+    div2(List,Div).
+div3(List,[]):-
+    length(List,0,Length), 
+    Length = 1.
+div3([El1,_,El2|Torig],[H|Tdiv]):-
+    Div1 is El1 - El2,
+    Div1 = H, 
+    div3([El2|Torig],Tdiv).
+div2([_,El1,_,El2|Torig],[H|Tdiv]):-
+    Div1 is El1 - El2,
+    Div1 = H,
+    div3([El2|Torig],Tdiv).
